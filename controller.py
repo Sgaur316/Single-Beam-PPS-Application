fcf126bf8f0a893289430dea1bede2cb3c1edfc6
Shriniwas Sharma <shriniwas.s@greyorange.sg>
2016-03-01 17:54:56 +0530

    Trying to make vertical control work

--- controller.py
+++ controller.py
@@ -3,10 +3,10 @@ import time
 from ola.ClientWrapper import ClientWrapper
 
 wrapper = None
-loop_count = 0
-TICK_INTERVAL = 40  # in ms
+TICK_INTERVAL = 400 # in ms
 
-global_count = 1
+global_count = 0
+pattern      = 0
 
 def DmxSent(state):
   if not state.Succeeded():
@@ -16,30 +16,40 @@ def SendDMXFrame():
   # schdule a function call in 100ms
   # we do this first in case the frame computation takes a long time.
   wrapper.AddEvent(TICK_INTERVAL, SendDMXFrame)
-  
-  controlMode    = 211
-  pattern        = 35
-  strobe         = 0
-  dotDisplay     = 100
-  horizontalMove = 0
-  verticalMove   = 225
-  zoom           = 0
-  # compute frame here
-  global loop_count
-  global global_count
-  # if loop_count%255 < 128 : 
-  verticalMove = global_count%160
-  horizontalMove = global_count%160
-  global_count = global_count + 4
-  time.sleep(0.5)
- 
-  loop_count += 1
+  global pattern
 
+  controlMode    = 211
+  pattern        = 0
+  strobe         = 0
+  dotDisplay     = 0
+  horizontalMove = 0
+  verticalMove   = 0
+  zoom           = 0
+  color          = 0
+  reset          = 0
+  color_1        = 0
+
+  # compute frame here
+  global global_count
+  # verticalMove = global_count%161 # 0-160
+  # if global_count % 161 == 0:
+  #  pattern += 1
+  verticalMove = global_count # 0-160
+  # horizontalMove = global_count%161 # 0-160
+
+  global_count += 10
+  global_count %= 161
+  time.sleep(1)
+  #horizontalMove = 222
+  
   # send
-  data = array.array('B', [controlMode, pattern, strobe, dotDisplay, horizontalMove, verticalMove, zoom])
-  print "data ", data
+  data = array.array('B', 
+    [controlMode, pattern, strobe, dotDisplay, horizontalMove, verticalMove, zoom, color, reset, color_1]
+  )
+
+  print "Pattern :", pattern, "data :", data
   wrapper.Client().SendDmx(1, data, DmxSent)
-wrapper = ClientWrapper()                                                                                                       
+wrapper = ClientWrapper() 
 while(1):
    SendDMXFrame()
 # wrapper.AddEvent(TICK_INTERVAL, SendDMXFrame)
