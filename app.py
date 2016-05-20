173ff4f88852f3bd68b3c64f9bfb023328650f07
Shriniwas Sharma <shriniwas.s@greyorange.sg>
2016-05-20 18:25:19 +0530

    Sends projector ID

--- app.py
+++ app.py
@@ -8,7 +8,15 @@ print 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 print 'Connected to server...'
 
+time.sleep(0.5)
+# Send connect packet with ID
+data = "pps_id, %s" % config.PPS_ID
+print "sending: ", data
+res = sock.send(data)
 
 while True:
 	print "Expecting a message from server..."
+	msg = sock.recv(4096)
+	print "Received message: ", msg
+	sock.send("abcd".encode())
 	time.sleep(2)
