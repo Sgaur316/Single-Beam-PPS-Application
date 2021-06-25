7e8a4cd9f29ea6a72d25c5ac6ef3f963aa95183a
Shivam Nagar <shivam.n@greyorange.com>
2021-06-25 17:43:54 +0530

    changes in usb_detection for multiple tty devices
    if multiple tty devices are connected then to choose the tty USB for projector only.

--- usb_detector.py
+++ usb_detector.py
@@ -21,12 +21,19 @@ def get_serial():
         # Make the list unique
         # Duplicates occur due to different USB versions supported(USB 1.0, 2.0 & 3.0)
         usb_devices = list(set(usb_devices))
-        if len(usb_devices) == 1:
-            logHandle.info("Usb Detector: Successful : Single unambiguous device found")
-            yield serial.Serial("/dev/" + str(usb_devices[0]))
+        if len(usb_devices) >= 1:
+            logHandle.info("Usb Detector: Successful : Checking for Projector Serial USB")
+            serial_obj = None
+            p_details = get_device_details()
+            for device in usb_devices:
+                serial_obj = serial.Serial("/dev/" + str(device))
+                if serial_obj.name == p_details.get("path", ""):
+                    break
+            yield serial_obj
             break
+
         else:
-            logHandle.info("Usb Detector: Error , more than one or no USB-DMX found retrying after 5 sec")
+            logHandle.info("Usb Detector: Error , no USB-DMX found retrying after 5 sec")
             yield None
             sleep(2)
             # continue
@@ -35,14 +42,16 @@ def get_serial():
 def get_device_details():
     response = {"name": "",
                 "model": "",
-                "serial_no": ""}
+                "serial_no": "",
+                "path": ""}
 
     try:
         for port in serial.tools.list_ports.comports():
-            if port.name == 'ttyUSB0':
-                response["name"] = str(port.name)
+            if str(port.product).find('DMX') != -1:
+                response["name"] = str(port.product)
                 response["serial_no"] = str(port.serial_number)
                 response["model"] = str(port.manufacturer)
+                response["path"] = str(port.name)
                 break
                 # print(port.hwid)
                 # print(port.location)
